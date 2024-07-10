644e782419910521510e5cd1f86615ed205eb054
Jnckk <riskicahgendeng@gmail.com>
2024-07-11 03:27:20 +0700

    add feature

--- route/fetchdataset.js
+++ route/fetchdataset.js
@@ -25,7 +25,7 @@ const fetchData = () => {
           travel2: data.travel2,
           travel3: data.travel3,
           travel4: data.travel4,
-          images: `https://github.com/Jnckk/Place-Recommendations-API/blob/main/Images/${data.place_id}.jpg`,
+          images: `https://enon8okxb2auhfev.public.blob.vercel-storage.com/Images/${data.place_id}.jpg`,
         });
       })
       .on("end", () => {
